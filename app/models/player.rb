586025bc4458ea0f1d702f329979cd5007b09cdc
Don Burks <don@donburks.com>
2016-06-09 13:33:32 -0700

    Code from morning exercise.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,16 +2,17 @@ class Player < ActiveRecord::Base
   belongs_to :team
 
   validates :name, presence: true
-  validates :jersey, presence: true, numericality: {only_integer: true, greater_than: 23}
-  #validate :test_team_capacity
-  before_create :test_team_capacity, if: :team
+  validates :jersey, presence: true, 
+    numericality: {
+      only_integer: true, 
+      greater_than: 23
+    }
+
+  before_create :check_team_capacity, if: :team
+  #validate :check_team_capacity
 
   private
-    def test_team_capacity
-      errors.add(:capacity, "team over capacity") if validate_team_capacity
-    end
-
-    def validate_team_capacity
-      team.players.length >= team.capacity
+    def check_team_capacity
+      errors[:capacity] << "team over capacity" if team.players.count >= team.capacity
     end
 end
