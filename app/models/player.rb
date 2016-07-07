3a03d1ba17d51a5cd168a6adb3c548a2ddad9328
Don Burks <don@donburks.com>
2016-07-07 11:00:29 -0700

    Code done in today's lecture

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,17 +1,22 @@
 class Player < ActiveRecord::Base
+  # .all - SELECT * FROM players;
+  # .find(#) - SELECT * FROM players WHERE id=#;
+  # .find_by(key: value) - SELECT * FROM players WHERE field=value LIMIT 1;
+  # .where(key: value) - SELECT * FROM players WHERE field=value;
+  # .where("field operator value") - SELECT * FROM players WHERE field operator value;
+
   belongs_to :team
 
   validates :name, presence: true
   validates :jersey, presence: true, numericality: {only_integer: true, greater_than: 23}
-  #validate :test_team_capacity
-  before_create :test_team_capacity, if: :team
+
+  validate :test_team_capacity, if: :team
 
   private
     def test_team_capacity
-      errors.add(:capacity, "team over capacity") if validate_team_capacity
-    end
-
-    def validate_team_capacity
-      team.players.length >= team.capacity
+      if team.players.count >= team.capacity
+        errors.add(:capacity, "team over capacity")
+      end
     end
 end
+
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ActiveRecord::Base
+  #SELECT * FROM players WHERE team_id=#{self.id};
   has_many :players, dependent: :destroy
 
   validates :name, presence: true, length: {minimum: 5}
   validates :city, presence: true
 end
+
